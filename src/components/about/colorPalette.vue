073c4e573a6bb3a441f36a05b1b74498d6eabf30
bigblackbexr <david-co31@hotmail.com>
2023-01-07 13:53:34 -0600

    the color palette is defined in the tailwind configuration file

--- src/components/about/colorPalette.vue
+++ src/components/about/colorPalette.vue
@@ -13,19 +13,19 @@
         </thead>
         <tbody>
           
-          <!-- PRIMARY -->
+          <!-- Pink -->
           <tr class="bg-background-light">
             <td class="px-12 py-2 rounded-l-full">
               <div class="mt-1">
-                Primary
+                Pink
               </div>
             </td>
             <td class="px-12 py-2">
-              <div class="flex items-center rounded-full bg-primary-light px-3 py-1.5">
+              <div class="flex items-center rounded-full bg-pink-light px-3 py-1.5">
                 <div class="mt-1 ml-2">
                   #FFBCE2
                 </div>
-                <div class="ml-2 rounded-full hover:ring-2 hover:ring-primary-dark px-3 py-1">
+                <div class="ml-2 rounded-full hover:ring-2 hover:ring-pink-dark px-3 py-1">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0118 9.375v9.375a3 3 0 003-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 00-.673-.05A3 3 0 0015 1.5h-1.5a3 3 0 00-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6zM13.5 3A1.5 1.5 0 0012 4.5h4.5A1.5 1.5 0 0015 3h-1.5z" clip-rule="evenodd" />
                     <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V9.375zM6 12a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V12zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 15a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V15zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 18a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V18zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
@@ -34,7 +34,7 @@
               </div>
             </td>
             <td class="px-12 py-2">
-              <div class="flex items-center rounded-full bg-primary px-3 py-1.5">
+              <div class="flex items-center rounded-full bg-pink px-3 py-1.5">
                 <div class="mt-1 ml-2">
                   #FFBCE2
                 </div>
@@ -47,11 +47,11 @@
               </div>
             </td>
             <td class="px-12 py-2 rounded-r-full">
-              <div class="flex items-center rounded-full bg-primary-dark px-3 py-1.5">
+              <div class="flex items-center rounded-full bg-pink-dark px-3 py-1.5">
                 <div class="mt-1 ml-2">
                   #FFBCE2
                 </div>
-                <div class="ml-2 rounded-full hover:ring-2 hover:ring-primary-light px-3 py-1">
+                <div class="ml-2 rounded-full hover:ring-2 hover:ring-pink-light px-3 py-1">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0118 9.375v9.375a3 3 0 003-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 00-.673-.05A3 3 0 0015 1.5h-1.5a3 3 0 00-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6zM13.5 3A1.5 1.5 0 0012 4.5h4.5A1.5 1.5 0 0015 3h-1.5z" clip-rule="evenodd" />
                     <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V9.375zM6 12a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V12zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 15a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V15zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 18a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V18zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
