87bd40086443d1597dfc4c5361c9c25b6ee154de
bigblackbexr <david-co31@hotmail.com>
2023-01-16 19:45:05 -0600

    dynamic colors by configuration

--- src/components/about/colorPalette.vue
+++ src/components/about/colorPalette.vue
@@ -13,7 +13,7 @@
           </tr>
         </thead>
         <tbody>
-          <Color :color="light" colorName="background" fill="silver" />
+          <Color fill="LightGrey" />
         </tbody>
       </table>
 	  </div>
@@ -21,76 +21,11 @@
 </template>
 
 <script>
-  import resolveConfig from 'tailwindcss/resolveConfig'
-  import tailwindConfig from 'tailwind-config'
-  const fullConfig = resolveConfig(tailwindConfig)
   import Color from './component/color.vue'
   
   export default {
     components: {
       Color
-    },
-    data () {
-      return {
-        background: {},
-        currentLine: {},
-        foreground: {},
-        comment: {},
-        cyan: {},
-        green: {},
-        orange: {},
-        pink: {},
-        purple: {},
-        red: {},
-        vampire: {},
-        yellow: {},
-
-        light: null,
-        default: null,
-        dark: null,
-      }
-    },
-    methods: {
-      getColors () {
-
-        let { background,
-              currentLine,
-              foreground,
-              comment,
-              cyan,
-              green,
-              orange,
-              pink,
-              purple,
-              red,
-              vampire,
-              yellow          
-            } = fullConfig.theme.colors
-
-        this.background = this.convertToUppercase(background)
-        this.currentLine = this.convertToUppercase(currentLine)
-        this.foreground = this.convertToUppercase(foreground)
-        this.comment = this.convertToUppercase(comment)
-        this.cyan = this.convertToUppercase(cyan)
-        this.green = this.convertToUppercase(green)
-        this.orange = this.convertToUppercase(orange)
-        this.pink = this.convertToUppercase(pink)
-        this.purple = this.convertToUppercase(purple)
-        this.red = this.convertToUppercase(red)
-        this.vampire = this.convertToUppercase(vampire)
-        this.yellow = this.convertToUppercase(yellow)
-
-        this.light = '[' + this.red.light + ']'
-        console.log(this.light);
-      },
-      convertToUppercase (object) {
-        return Object.fromEntries(
-          Object.entries(object).map(([key, value]) => [key, value.toUpperCase()])
-        )
-      },
-    },
-    mounted () {
-      this.getColors()
     }
   }
 </script>
