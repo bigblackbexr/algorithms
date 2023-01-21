04556832405c731887776c691fb2f080dbe28372
bigblackbexr <david-co31@hotmail.com>
2023-01-20 22:28:27 -0600

    Init navbar

---
+++ src/components/nav.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="bg-cyan rounded m-4 flex items-center text-sm">
+		<div>
+			<span> Algorithms </span>
+		</div>
+		<div class="block" @click="a()">
+			<button>
+				B
+			</button>
+		</div>
+		<div>
+			<a href="/">
+				a
+			</a>
+			<a href="/">
+				b
+			</a>
+			<a href="/">
+				c
+			</a>
+		</div>
+		<div>
+			<a href="/">Download</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	methods:{
+		a () {
+			console.log('hola');
+		}
+	}
+}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<nav>
+		<Nav />
+	</nav>
+	<h1> Mundo </h1>
+</template>
+
+<script setup>
+	import Nav from '../components/nav.vue'
+</script>
