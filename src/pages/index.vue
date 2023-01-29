222ec732c72bb7c9983a7dcab3ba2b5d3b8fa08d
bigblackbexr <david-co31@hotmail.com>
2023-01-29 11:08:00 -0600

    start with new footer

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,16 @@
 <template>
+	
 	<nav>
 		<Nav />
 	</nav>
+
+	<footer>
+		<Foot />
+	</footer>
+
 </template>
 
 <script setup>
 	import Nav from '../components/nav.vue'
+	import Foot from '../components/footer.vue'
 </script>
