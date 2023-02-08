213b42b630b9e9b33485aea77b2ffb6fb4619b14
bigblackbexr <david-co31@hotmail.com>
2023-02-07 23:33:19 -0600

    fixes to mobile

--- src/components/main.vue
+++ src/components/main.vue
@@ -8,12 +8,12 @@
             rounded-xl text-5xl p-10 pb-0
             max-md:text-4xl max-md:p-8 max-md:pb-0"
           >
-            <div class="bg-logo-light dark:bg-logo-dark mx-auto w-52 h-52">
+            <div class="bg-logo-light dark:bg-logo-dark mx-auto w-56 h-56 bg-center bg-no-repeat">
               &nbsp
             </div>
             Algorithms
           </div>
-          <div class="text-md ml-48 max-md:ml-40">
+          <div class="text-md ml-48 max-md:ml-32">
             <span class="relative">
               v0.0
               <span class="animate-pulse absolute mt-0.5 ml-0.5 text-4xl rounded-full bg-green-light dark:bg-purple-light h-1.5 w-1.5"> </span>
