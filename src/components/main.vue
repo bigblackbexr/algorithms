18a3069ddf65fc2e8dd2a265fd89ad95cac7093d
David C <david-co31@hotmail.com>
2023-02-11 23:47:40 -0600

    fixes

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,21 +2,20 @@
   <div class="h-full">
     <div>
       <nav class="grid place-items-center -my-10">
-
         
           <div class="
-            rounded-xl text-5xl p-10 pb-0
-            max-md:text-4xl max-md:p-8 max-md:pb-0"
+            rounded-xl text-4xl p-10 pb-0
+            max-md:p-8 max-md:pb-0"
           >
-            <div class="bg-logo-light dark:bg-logo-dark mx-auto w-56 h-56 bg-center bg-no-repeat">
+            <div class="bg-logo-light dark:bg-logo-dark mx-auto w-40 h-40 bg-center bg-no-repeat">
               &nbsp
             </div>
             Algorithms
           </div>
-          <div class="text-md ml-48 max-md:ml-32">
-            <span class="relative">
+          <div class="text-md ml-36 max-md:ml-28">
+            <span class="relative text-base">
               v0.0
-              <span class="animate-pulse absolute mt-0.5 ml-0.5 text-4xl rounded-full bg-green-light dark:bg-purple-light h-1.5 w-1.5"> </span>
+              <span class="animate-pulse absolute mt-0.5 ml-0.5 rounded-full bg-green-light dark:bg-purple-light h-1.5 w-1.5"> </span>
             </span>
           </div>
 
