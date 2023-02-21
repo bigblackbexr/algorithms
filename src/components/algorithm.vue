76213f8cab995000204cd00298d2c6a82c04b275
David C <david-co31@hotmail.com>
2023-02-20 23:46:51 -0600

    add page to algorithm detail

---
+++ src/components/algorithm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1 class="text-red">
+      Hola
+    </h1>
+    <h2 class="text-orange">
+      Mundo
+    </h2>
+    <h3 class="text-yellow">
+      Cruel
+    </h3>
+  </div>
+</template>
