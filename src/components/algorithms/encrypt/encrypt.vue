b199fab3526671a9cd84918b3f7d0a41eea4e59a
David C <david-co31@hotmail.com>
2023-03-02 20:40:13 -0600

    encrypt description & install animejs

--- src/components/algorithms/encrypt/encrypt.vue
+++ src/components/algorithms/encrypt/encrypt.vue
@@ -11,6 +11,14 @@
       </pre>    
     </h1>
 
+    <p>
+      Un algoritmo de encriptación es un conjunto de reglas matemáticas y procedimientos que se utilizan para transformar información legible (texto plano) en información ilegible (texto cifrado) de manera que sólo las personas autorizadas puedan descifrar y leer la información original.
+
+      Los algoritmos de encriptación se utilizan para proteger la confidencialidad de la información, especialmente en las comunicaciones electrónicas y el almacenamiento de datos. Los algoritmos de encriptación pueden ser simétricos o asimétricos.
+
+      En los algoritmos simétricos, se utiliza la misma clave para cifrar y descifrar la información. En los algoritmos asimétricos, se utilizan dos claves diferentes: una clave pública para cifrar la información y una clave privada para descifrarla. Los algoritmos asimétricos son más seguros que los simétricos, pero también son más complejos y requieren más recursos computacionales.
+    </p>
+
 
   </div>
 </template>
