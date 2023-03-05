fe186b800ff7cf9673563d2ecaa49aecc63908b4
David C <david-co31@hotmail.com>
2023-03-04 19:53:23 -0600

    first design of algorithm detail's

--- src/components/algorithms/encrypt/encrypt.vue
+++ src/components/algorithms/encrypt/encrypt.vue
@@ -11,11 +11,8 @@
       </pre>    
     </h1>
 
-    <div class="mx-10 max-md:mx-6 my-5 text-cyan text-base text-justify">
-      <span class="description"></span>
-      <ol class="pl-8">
-        <li class="list"></li>
-      </ol>
+    <div class="mx-10 max-md:mx-6 my-5 text-cyan text-xl max-md:text-base text-justify">
+      <span class="define" />
     </div>
 
   </div>
@@ -28,43 +25,39 @@
     data () {
       return {
         item: null,
-        items: ['🤖','🐻','🖤','👽' ],
+        items: ['🌕','🌖','🌗','🌘','🌑','🌒','🌓','🌔']
       }
     },
-    methods: {
-      cursor () {
-        const cur = document.querySelector('.typed-cursor')
-        if(cur)
-        {cur.style.display = 'none'}
-      }
-    }
-    ,
     mounted () {
       this.item = this.items[Math.floor(Math.random()*this.items.length)]
 
-      let textOne = 'An encryption algorithm is a set of mathematical instructions used to encode information to make it unreadable for anyone without the decryption key. <br> <br>'
-      let textTwo = 'Encryption algorithms are widely used in information security to protect confidential data from unauthorized access. <br> <br>'
-      let textThree = 'There are two main types of encryption algorithms: <br> <br>'
-      let textFour = '1.Symmetric Encryption: also known as secret-key cryptography, uses the same key to encrypt and decrypt information. This type of encryption is commonly used in file storage and communication systems where both parties share a secret key, such as passwords or personal data. <br> <br>'
-      let textFive = '2.Asymmetric Encryption: also known as public-key cryptography, uses a public key and a private key to encrypt and decrypt information. The public key is widely shared and used to encrypt the data, while the private key is kept secure and used to decrypt the data. This type of encryption is commonly used in user authentication, digital signatures, and secure online transactions. <br>'
+      let a = 'An encryption algorithm is a set of mathematical instructions used to encode information to make it unreadable for anyone without the decryption key. <br> <br>'
+      let b = 'Encryption algorithms are widely used in information security to protect confidential data from unauthorized access. <br> <br>'
+      let c = 'There are two main types of encryption algorithms: <br> <br>'
 
-      let finalText = textOne + textTwo + textThree
+      let d = '<p class="px-12 max-md:px-6 text-pink">Symmetric Encryption</p>'
+      let e = '<p class="px-12 max-md:px-6"> Also known as secret-key cryptography, uses the same key to encrypt and decrypt information. This type of encryption is commonly used in file storage and communication systems where both parties share a secret key, such as passwords or personal data. </p> <br>'
+      let f = '<p class="px-12 max-md:px-6 text-pink">Asymmetric Encryption</p>'
+      let g = '<p class="px-12 max-md:px-6"> Also known as public-key cryptography, uses a public key and a private key to encrypt and decrypt information. The public key is widely shared and used to encrypt the data, while the private key is kept secure and used to decrypt the data. This type of encryption is commonly used in user authentication, digital signatures, and secure online transactions. </p> <br>'
 
-      new Typed('.description', {
-        strings: [finalText],
-        typeSpeed: 40,
+      let h = 'Encryption algorithms are used in a wide variety of use cases, including: <br> <br>'
+      let i = '<p class="px-14 max-md:px-2 text-center text-orange">1.User Data Protection</p>'
+      let j = '<p class="px-14 max-md:px-2 text-center text-orange">2.Secure Communications</p>'
+      let k = '<p class="px-14 max-md:px-2 text-center text-orange">3.Online Payment Systems</p>'
+      let l = '<p class="px-14 max-md:px-2 text-center text-orange">4.File and Disk Protection</p> <br>'
+
+      let m = 'Overall, encryption algorithms are an important part of information security today, and their use is critical to protecting privacy and confidentiality of online data.'
+
+      const textA = a + b + c
+      const textB = d + e + f + g
+      const textC = h + i + j + k + l
+      const textD = m
+
+      new Typed('.define', {
+        strings: [textA + textB + textC + textD],
+        typeSpeed: 25,
         cursorChar: this.item,
-        onComplete: () => {
-
-          this.cursor()
-
-          new Typed('.list', {
-            strings: [textFour + textFive],
-            typeSpeed: 40,
-            cursorChar: this.item
-          })
-          
-        }
+        loop: false
       })
     }
   }
