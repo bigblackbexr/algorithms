efcbead9ed6a74369ac5dfa7a1abb64d37302a42
David C <david-co31@hotmail.com>
2023-03-18 23:43:18 -0600

    in index.js no

--- src/components/algorithms/no/index.js
+++ src/components/algorithms/no/index.js
@@ -1,3 +1,13 @@
 // https://github.com/trekhleb/javascript-algorithms/blob/master/README.es-ES.md
 
 //https://the-algorithms.com/
+
+/*
+Torre de Hanói
+Rotación de matriz cuadrada - algoritmo in situ
+Juego de los saltos - ejemplos de backtracking, programación dinámica (de arriba hacia abajo + de abajo hacia arriba) y voraces
+Caminos únicos - ejemplos de backtracking, programación dinámica y basados en el Triángulo de Pascal
+Terrazas pluviales - el problema de la retención del agua de lluvia (programación dinámica y fuerza bruta)
+Problema de las N Reinas
+Problema del caballo (Knight tour)
+*/
