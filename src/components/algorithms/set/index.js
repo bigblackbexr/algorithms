345c9a3ffd0c4cae111da0bf664e5aefe7d16f1c
David C <david-co31@hotmail.com>
2023-03-24 23:30:15 -0600

    algorithms on spanish to set

--- src/components/algorithms/set/index.js
+++ src/components/algorithms/set/index.js
@@ -1,3 +1,19 @@
 // https://github.com/trekhleb/javascript-algorithms/blob/master/README.es-ES.md
 
 //https://the-algorithms.com/
+
+/*
+
+Producto cartesiano
+Permutación de Fisher–Yates
+Conjunto potencia
+Permutaciones
+Combinaciones
+Subsecuencia común más larga
+Subsecuencia creciente más larga
+Supersecuencia común más corta
+Problema de la mochila 
+Máximo subarreglo
+Suma combinada
+
+*/
