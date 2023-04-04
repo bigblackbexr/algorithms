d069315b0ed6e9b621af65586422ffd8df620ca0
david-Cort3s <david-co31@hotmail.com>
2023-04-03 20:51:48 -0600

    list of sort algorithms added

--- src/components/algorithms/sort/index.js
+++ src/components/algorithms/sort/index.js
@@ -1,3 +1,27 @@
 // https://github.com/trekhleb/javascript-algorithms/blob/master/README.es-ES.md
 
 //https://the-algorithms.com/
+
+/*
+
+Ordenamiento de burbuja
+Ordenamiento por selección
+Ordenamiento por inserción
+Ordenamiento por Heap
+Ordenamiento por mezcla
+Quicksort - implementaciones in situ y no in situ
+Shellsort
+Ordenamiento por cuentas
+Ordenamiento Radix
+
+bubble
+by-selection
+insertion
+Heap
+Merge
+Quicksort
+Shellsort
+by-accounts
+radix
+
+*/
