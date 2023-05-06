aa9e8c022a14b1d09f78af79eebcf7743d5d6bb3
david-Cort3s <david-co31@hotmail.com>
2023-05-06 00:00:49 -0600

    Index of string category set

--- src/components/algorithms/string/string.vue
+++ src/components/algorithms/string/string.vue
@@ -22,7 +22,7 @@
 
       <div class="grid grid-cols-2 gap-4 max-md:gap-2 place-content-stretch justify-items-center
                   max-md:flex max-md:flex-col text-center">
-        <RouterLink :to="`sort/${algorithm}`" v-for="algorithm in algorithms" 
+        <RouterLink :to="`string/${algorithm}`" v-for="algorithm in algorithms"
           class="w-full rounded-lg max-md:text-sm p-4 max-md:p-2 bg-orange-light text-orange-dark dark:bg-purple-light dark:text-vampire-dark
                  hover:animate-pulse">
             <p class="capitalize">
@@ -43,15 +43,7 @@
     data () {
       return {
         algorithms: [
-          'accounts',
-          'bubble',
-          'heap',
-          'insertion',
-          'merge',
-          'quicksort',
-          'radix',
-          'selection',
-          'shellsort'
+          'another',
         ]
       }
     }
