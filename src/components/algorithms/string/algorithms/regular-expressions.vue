510bcb0b8622246691030bc538c68251ffbe675c
david-Cort3s <david-co31@hotmail.com>
2023-05-22 18:10:33 -0600

    r-e algorithm added to string category

--- src/components/algorithms/string/algorithms/regular-expressions.vue
+++ src/components/algorithms/string/algorithms/regular-expressions.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>Hola</h1>
+  <div>
+
+    <Base />
+
+    <div class="absolute inset-x-4 top-40 bottom-24 overflow-auto">
+
+      <div class="relative max-md:mr-2">
+
+        <div class="max-md:mx-0 mx-4">
+          <div class="rounded-lg border-solid border-2 dark:border-purple-light">
+            <div class="m-2">
+
+              <p class="text-orange-dark dark:text-purple-light">
+                Regular Expressions
+              </p>
+              <br>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit quod fugit provident quis error est, eum, vero, architecto molestias repellendus pariatur voluptatem ab quasi dicta impedit animi commodi. Nobis, fugit.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit quod fugit provident quis error est, eum, vero, architecto molestias repellendus pariatur voluptatem ab quasi dicta impedit animi commodi. Nobis, fugit.
+              </p>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
 </template>
+
+<script>
+
+  import Base from '../../base.vue'
+
+  export default {
+    components: {
+      Base
+    }
+  }
+
+</script>
