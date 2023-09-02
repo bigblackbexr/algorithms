c46a39f85e676214094ac1d0ba7d5249e41e9928
david-Cort3s <david-co31@hotmail.com>
2023-09-02 10:34:48 -0600

    base of routes from search category ready

---
+++ src/router/categories/search/linear.js
@@ -0,0 +1,11 @@
+import ñComponent from '@/components/algorithms/no/algorithms/z.vue';
+
+const ñRoute = [
+  {
+    path: '/no/z',
+    name: 'z',
+    component: ñComponent
+  },
+];
+
+export default ñRoute;
