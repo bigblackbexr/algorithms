07f7cf3a5e692722b4aa6e642e74c3185891521f
david-Cort3s <david-co31@hotmail.com>
2023-09-04 10:46:51 -0600

    All routes to set router

---
+++ src/router/categories/set/longest-growing-subsequence.js
@@ -0,0 +1,11 @@
+import ñComponent from '@/components/algorithms/set/algorithms/-.vue';
+
+const ñRoute = [
+  {
+    path: '/set/-',
+    name: '-',
+    component: ñComponent
+  },
+];
+
+export default ñRoute;
