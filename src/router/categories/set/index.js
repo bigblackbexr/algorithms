ab02ca9eccb97e6062b91362df868a0e24e49127
david-Cort3s <david-co31@hotmail.com>
2023-09-07 07:38:23 -0600

    routes of all set category set

--- src/router/categories/set/index.js
+++ src/router/categories/set/index.js
@@ -1,5 +1,17 @@
 import set from "@/components/algorithm.vue";
 
+import backpackProblemRoute from "./backpack-problem";
+import cartesianProductRoute from "./cartesian-product";
+import combinationsRoute from "./combinations";
+import combinedSumRoute from "./combined-sum";
+import fisherYatesPermutationRoute from "./fisher–yates-permutation";
+import longestCommonSubsequenceRoute from "./longest-common-subsequence";
+import longestGrowingSubsequenceRoute from "./longest-growing-subsequence";
+import maximumSubarrayRoute from "./maximum-subarray";
+import permutationsRoute from "./permutations";
+import setPowerRoute from "./set-power";
+import shortestCommonSupersequenceRoute from "./shortest-common-supersequence";
+
 const setCategory = [
   {
     path: '/set',
@@ -7,6 +19,17 @@ const setCategory = [
     component: set,
     props: true
   },
+  ...backpackProblemRoute,
+  ...cartesianProductRoute,
+  ...combinationsRoute,
+  ...combinedSumRoute,
+  ...fisherYatesPermutationRoute,
+  ...longestCommonSubsequenceRoute,
+  ...longestGrowingSubsequenceRoute,
+  ...maximumSubarrayRoute,
+  ...permutationsRoute,
+  ...setPowerRoute,
+  ...shortestCommonSupersequenceRoute,
 ];
 
 export default setCategory;
