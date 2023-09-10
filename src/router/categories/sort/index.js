2649af7bd6435dacff77324a17d9b7a2e6a4efbc
david-Cort3s <david-co31@hotmail.com>
2023-09-09 18:50:01 -0600

    index of sort category ready

---
+++ src/router/categories/sort/index.js
@@ -0,0 +1,12 @@
+import sort from "@/components/algorithm.vue";
+
+const sortCategory = [
+  {
+    path: '/sort',
+    name: 'sort',
+    component: sort,
+    props: true
+  },
+];
+
+export default sortCategory;
