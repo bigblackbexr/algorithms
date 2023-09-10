d3475f9e81be1ed3b3d83bd9616a9e607ebecdc1
david-Cort3s <david-co31@hotmail.com>
2023-09-09 18:53:17 -0600

    all algorithms files ready

---
+++ src/router/categories/sort/quicksort.js
@@ -0,0 +1,11 @@
+import ñComponent from '@/components/algorithms/sort/algorithms/ñ.vue';
+
+const ñRoute = [
+  {
+    path: '/sort/ñ',
+    name: 'ñ',
+    component: ñComponent
+  },
+];
+
+export default ñRoute;
