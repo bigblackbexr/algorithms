fee07dbe1d3a2cabe1c004054bdcffa6ba1be452
david-Cort3s <david-co31@hotmail.com>
2023-09-10 12:32:41 -0600

    base of all file algorithms ready

---
+++ src/router/categories/string/regular-expressions.js
@@ -0,0 +1,11 @@
+import ñComponent from '@/components/algorithms/encrypt/algorithms/-.vue';
+
+const ñRoute = [
+  {
+    path: '/encrypt/-',
+    name: '-',
+    component: ñComponent
+  },
+];
+
+export default ñRoute;
