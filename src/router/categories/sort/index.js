47d670d4f1e39caa3155219459fa743bfba4df3d
david-Cort3s <david-co31@hotmail.com>
2023-09-09 18:57:51 -0600

    sort category ready

--- src/router/categories/sort/index.js
+++ src/router/categories/sort/index.js
@@ -1,5 +1,15 @@
 import sort from "@/components/algorithm.vue";
 
+import accountsRoute from "./accounts";
+import bubbleRoute from "./bubble";
+import heapRoute from "./heap";
+import insertionRoute from "./insertion";
+import mergeRoute from "./merge";
+import quicksortRoute from "./quicksort";
+import radixRoute from "./radix";
+import selectionRoute from "./selection";
+import shellsortRoute from "./shellsort";
+
 const sortCategory = [
   {
     path: '/sort',
@@ -7,6 +17,15 @@ const sortCategory = [
     component: sort,
     props: true
   },
+  ...accountsRoute,
+  ...bubbleRoute,
+  ...heapRoute,
+  ...insertionRoute,
+  ...mergeRoute,
+  ...quicksortRoute,
+  ...radixRoute,
+  ...selectionRoute,
+  ...shellsortRoute,
 ];
 
 export default sortCategory;
