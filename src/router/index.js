3d564a0bd478d6e3103bdfc07cc23e963076c975
david-Cort3s <david-co31@hotmail.com>
2023-09-11 20:41:44 -0600

    organize route finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import allRoutes from "./categories/index";
 
-console.log(allRoutes, 'aquitoy');
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     ...allRoutes,
 
     // INDEX
-
     {
       path: '/',
       name: 'index',
