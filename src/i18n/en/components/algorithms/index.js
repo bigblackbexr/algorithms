4da782aaab850fea7c8bba02a5fb10fe3a1ebe77
david-Cort3s <david-co31@hotmail.com>
2023-09-26 19:50:48 -0600

    index and base of sub folder algorithms ready

---
+++ src/i18n/en/components/algorithms/index.js
@@ -0,0 +1,12 @@
+import base from "./base"
+import encrypt from "./encrypt/index"
+import graph from "./graph/index"
+import list from "./list/index"
+
+export default {
+  index: 'indexAlgorithms',
+  base,
+  encrypt,
+  graph,
+  list
+}
