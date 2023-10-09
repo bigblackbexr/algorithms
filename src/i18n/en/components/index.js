57354943bf11beeabc0249eeebca40434e957d75
david-Cort3s <david-co31@hotmail.com>
2023-10-08 23:09:32 -0600

    english ready

--- src/i18n/en/components/index.js
+++ src/i18n/en/components/index.js
@@ -3,9 +3,11 @@ import footer from "./footer";
 import main from "./main";
 import nav from "./nav";
 import Index from './algorithms/index'
+import about from "./about";
 
 export default {
   algorithm,
+  about,
   footer,
   index: 'index',
   Index,
