ef98a57d7c3626500ba471be7d17fe76e1f554f0
david-Cort3s <david-co31@hotmail.com>
2023-10-12 23:45:45 -0600

    tree category to spanish

---
+++ src/i18n/es/components/algorithms/index.js
@@ -0,0 +1,26 @@
+import base from "./base"
+import encrypt from "./encrypt"
+import graph from "./graph"
+import list from "./list"
+import math from "./math"
+import no from "./no"
+import search from "./search"
+import set from "./set"
+import sort from "./sort"
+import string from "./string"
+import tree from "./tree"
+
+export default {
+  base,
+  // encrypt,
+  // graph,
+  index: 'indexAlgorithms',
+  // list,
+  // math,
+  // no,
+  // search,
+  // set,
+  // sort,
+  // string,
+  tree,
+};
