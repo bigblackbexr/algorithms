58f15d43e8e903d0dd454aa84ef24c2f27870e7c
david-Cort3s <david-co31@hotmail.com>
2023-10-15 15:18:43 -0600

    graph ready

---
+++ src/i18n/es/components/algorithms/graph/index.js
@@ -0,0 +1,33 @@
+import bellmanFord from "./algorithms/bellman-ford";
+import bridges from "./algorithms/bridges";
+import depthSearch from "./algorithms/depth-search";
+import detectCycles from "./algorithms/detect-cycles";
+import dijkstra from "./algorithms/dijkstra";
+import eulerianPathCircuit from "./algorithms/eulerian-path-circuit";
+import floydWarshall from "./algorithms/floyd-warshall";
+import hamiltonianCycle from "./algorithms/hamiltonian-cycle";
+import kruskal from "./algorithms/kruskal";
+import pointsArticulation from "./algorithms/points-articulation";
+import prim from "./algorithms/prim";
+import stronglyConnectedComponents from "./algorithms/strongly-connected-components";
+import topologicalOrdering from "./algorithms/topological-ordering";
+import travelerProblem from "./algorithms/traveler-problem";
+import widthSearch from "./algorithms/width-search";
+
+export default {
+  bellmanFord,
+  bridges,
+  depthSearch,
+  detectCycles,
+  dijkstra,
+  eulerianPathCircuit,
+  floydWarshall,
+  hamiltonianCycle,
+  kruskal,
+  pointsArticulation,
+  prim,
+  stronglyConnectedComponents,
+  topologicalOrdering,
+  travelerProblem,
+  widthSearch
+}
