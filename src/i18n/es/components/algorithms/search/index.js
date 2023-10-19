6fcda37f70e45a8c146802b525a759930422a085
david-Cort3s <david-co31@hotmail.com>
2023-10-19 06:53:30 -0600

    search category ready

---
+++ src/i18n/es/components/algorithms/search/index.js
@@ -0,0 +1,11 @@
+import binary from "./algorithms/binary";
+import interpolation from "./algorithms/interpolation";
+import jump from "./algorithms/jump";
+import linear from "./algorithms/linear";
+
+export default {
+  binary,
+  interpolation,
+  jump,
+  linear
+};
