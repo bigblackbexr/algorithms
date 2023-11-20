66e09769d97f3da48046c0601e17e81b63094a4c
david-Cort3s <david-co31@hotmail.com>
2023-11-20 14:04:46 -0600

    add list description to i18n

--- src/i18n/en/components/algorithms/list/index.js
+++ src/i18n/en/components/algorithms/list/index.js
@@ -1,7 +1,9 @@
-import directRoute from "./algorithms/direct-route"
-import invertRoute from "./algorithms/invert-route"
+import list from "./list";
+import directRoute from "./algorithms/direct-route";
+import invertRoute from "./algorithms/invert-route";
 
 export default {
+  list,
   directRoute,
   invertRoute
 }
