c97d958a14598700eebb2c677528f14937671bfc
david-Cort3s <david-co31@hotmail.com>
2023-11-22 11:36:47 -0600

    add list category def in english

--- src/i18n/en/components/algorithms/list/list.js
+++ src/i18n/en/components/algorithms/list/list.js
@@ -9,48 +9,49 @@ const classes = {
 
 const p1 = {
   a:
-    `A graph algorithm is a set of rules and mathematical procedures designed to analyze and manipulate data
-    represented in the form of a graph. A graph is a structure composed of nodes (vertices) and connections (edges) that represent
-    relationships between these nodes. <br> <br>`,
+    `A list algorithm comprises a set of principles and computational procedures crafted for the analysis and manipulation of data
+    organized in the format of a list. A list, in this context, stands as a sequential arrangement of elements, each uniquely
+    identified by its index or position. <br> <br>`,
   b:
-    'These algorithms are essential in a variety of fields and applications where data with a graph structure is handled. <br> <br>',
+    'These algorithms play a pivotal role in diverse fields and applications where data exhibits a list-based structure. <br> <br>',
   c:
-    'There are two main categories of graph algorithms: <br> <br>'
+    'There exist two primary categories of list algorithms: <br> <br>'
 }
 
 const p2 = {
   a: `
-    <p class="${classes.cP2}">Search and Exploration</p>`,
+    <p class="${classes.cP2}">Exploration and Identification</p>`,
   b:
-    `<p class="${classes.paddingP2}">These algorithms are used to search for paths, relationships, or specific features within
-    a graph. They can be used to determine if there is a path between two nodes, find the shortest path between nodes, or explore
-    the complete structure of the graph. </p> <br>`,
+    `<p class="${classes.paddingP2}">These algorithms serve the purpose of seeking out specific elements or characteristics within
+    a given list. They can ascertain the presence of an element, pinpoint its position within the list, or extract elements that
+    meet predefined criteria. </p> <br>`,
   c:
-    `<p class="${classes.cP2}">Processing and Analysis</p>`,
+    `<p class="${classes.cP2}">Arrangement and Transformation</p>`,
   d:
-    `<p class="${classes.paddingP2}">These algorithms focus on extracting meaningful information from graph data. They may include
-    community detection algorithms to identify groups of nodes with strong connections, centrality analysis algorithms to find key nodes in the graph, or clustering algorithms to group similar nodes. </p> <br>`
+    `<p class="${classes.paddingP2}">This category focuses on the systematic organization of elements or the execution of operations
+    on the list. Sorting algorithms, for instance, streamline the arrangement of elements in either ascending or descending order.
+    Meanwhile, transformative algorithms can include tasks such as the addition or removal of elements. </p> <br>`
 }
 
 const p3 = {
   a:
-    `Graph algorithms are applied in various areas, including: <br> <br>`,
+    `List algorithms find practical applications across various domains, including: <br> <br>`,
   b:
-    `<p class="${classes.cp3}">1. Social Networks</p>`,
+    `<p class="${classes.cp3}">1. Data Governance Systems</p>`,
   c:
-    `<p class="${classes.cp3}">2. Transportation and Logistics</p>`,
+    `<p class="${classes.cp3}">2. Knowledge Retrieval</p>`,
   d:
-    `<p class="${classes.cp3}">3. Computational Biology</p>`,
+    `<p class="${classes.cp3}">3. Automated Task and Scheduling</p>`,
   e:
-    `<p class="${classes.cp3}">4. Content Recommendation</p>`,
+    `<p class="${classes.cp3}">4. Textual Processing and Analysis</p>`,
   f:
-    `<p class="${classes.cp3}">5. Network Analysis</p> <br>`
+    `<p class="${classes.cp3}">5. Inventory Oversight</p> <br>`
 }
 
 const p4 = {
   a:
-    `In general, graph algorithms are essential tools for analyzing and understanding data represented in the form of a graph,
-    and their versatility makes them fundamental elements in a wide variety of applications and disciplines.`
+    `In essence, list algorithms represent indispensable tools for dissecting and reshaping data manifesting in list form. Their
+    adaptability positions them as essential components within a myriad of applications spanning different disciplines.`
 }
 
 const defineList = {
