71de6546a46e8bed763beb1d62c1043c12488f64
David Cortés <david.cortes@prouniversitaria.org>
2023-11-26 16:12:21 -0600

    config classes file to definition of each category of algorithm

--- src/utils/classes-definition.js
+++ src/utils/classes-definition.js
@@ -3,6 +3,6 @@ const classes = {
   textColorP2: 'text-red dark:text-pink',
   cP2: 'px-12 max-md:px-6 text-red dark:text-pink',
   cp3: 'px-14 max-md:px-2 text-center text-red-dark dark:text-pink-light'
-}
+};
 
 export default classes;
