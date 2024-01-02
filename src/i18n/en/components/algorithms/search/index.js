6771d1a557934c1346193a3dcfc5f7976f2699b0
David Cortés <david.cortes@prouniversitaria.org>
2024-01-01 22:26:03 -0600

    search def in js file added in index!

--- src/i18n/en/components/algorithms/search/index.js
+++ src/i18n/en/components/algorithms/search/index.js
@@ -1,11 +1,13 @@
+import search from "./search";
 import binary from "./algorithms/binary";
 import interpolation from "./algorithms/interpolation";
 import jump from "./algorithms/jump";
 import linear from "./algorithms/linear";
 
 export default {
+  search,
   binary,
   interpolation,
   jump,
-  linear
+  linear,
 };
