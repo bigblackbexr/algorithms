eff7c32e42bdcc110f935d779ad2b4dd4d6e1a41
David Cortés <david.cortes@prouniversitaria.org>
2024-01-02 22:45:06 -0600

    Search def in english base ready

---
+++ src/i18n/en/components/algorithms/search/search.js
@@ -0,0 +1,13 @@
+import joinProperties from "@/utils/join-properties";
+import classes from "@/utils/classes-definition";
+
+const defineSearch = {
+  // a: joinProperties(p1),
+  // b: joinProperties(p2),
+  // c: joinProperties(p3),
+};
+
+const defSearch = "Que cagado";
+// const defNo = joinProperties(defineSearch);
+
+export default defSearch;
