44790194ce1f2e162a697ba0f90b14cb1b260bff
david-Cort3s <david-co31@hotmail.com>
2024-01-11 23:25:56 -0600

    Set prettier

--- src/components/algorithms/set/set.vue
+++ src/components/algorithms/set/set.vue
@@ -1,62 +1,63 @@
 <template>
   <div>
-
-    <br>
+    <br />
     <h1>
-      <pre class="grid place-content-center -mt-18 max-md:-mt-0 text-xl max-md:text-[8px] max-md:leading-[10px]">
+      <pre class="-mt-18 grid place-content-center text-xl max-md:-mt-0 max-md:text-[8px] max-md:leading-[10px]">
 ███████╗███████╗████████╗
 ██╔════╝██╔════╝╚══██╔══╝
-███████╗█████╗     ██║   
-╚════██║██╔══╝     ██║   
-███████║███████╗   ██║   
-╚══════╝╚══════╝   ╚═╝   
-      </pre>    
+███████╗█████╗     ██║
+╚════██║██╔══╝     ██║
+███████║███████╗   ██║
+╚══════╝╚══════╝   ╚═╝
+      </pre>
     </h1>
 
-    <div class="text-orange-dark dark:text-purple-light mx-10 max-md:mx-6 my-5 text-xl max-md:text-base text-justify">
+    <div class="mx-10 my-5 text-justify text-xl text-orange-dark max-md:mx-6 max-md:text-base dark:text-purple-light">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae labore sit eius voluptates molestiae eligendi, reiciendis, hic nisi dolores doloremque quasi cupiditate explicabo rem quidem veniam nulla vel molestias.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae labore sit eius voluptates molestiae
+        eligendi, reiciendis, hic nisi dolores doloremque quasi cupiditate explicabo rem quidem veniam nulla vel
+        molestias.
       </p>
 
-      <br>
+      <br />
 
-      <div class="grid grid-cols-2 gap-4 max-md:gap-2 place-content-stretch justify-items-center
-                  max-md:flex max-md:flex-col text-center">
-        <RouterLink :to="`set/${algorithm}`" v-for="algorithm in algorithms" 
-          class="w-full rounded-lg max-md:text-sm p-4 max-md:p-2 bg-orange-light text-orange-dark dark:bg-purple-light dark:text-vampire-dark
-                 hover:animate-pulse">
-            <p class="capitalize">
-              {{ algorithm }}
-            </p>
-        </RouterLink >
+      <div
+        class="grid grid-cols-2 place-content-stretch justify-items-center gap-4 text-center max-md:flex max-md:flex-col max-md:gap-2"
+      >
+        <RouterLink
+          :to="`set/${algorithm}`"
+          v-for="algorithm in algorithms"
+          class="w-full rounded-lg bg-orange-light p-4 text-orange-dark hover:animate-pulse max-md:p-2 max-md:text-sm dark:bg-purple-light dark:text-vampire-dark"
+        >
+          <p class="capitalize">
+            {{ algorithm }}
+          </p>
+        </RouterLink>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-
-  import { RouterLink } from 'vue-router'
+  import { RouterLink } from "vue-router";
 
   export default {
-    data () {
+    data() {
       return {
         algorithms: [
-          'backpack-problem',
-          'cartesian-product',
-          'combinations',
-          'combined-sum',
-          'fisher–yates-permutation',
-          'longest-common-subsequence',
-          'longest-growing-subsequence',
-          'maximum-subarray',
-          'permutations',
-          'set-power',
-          'shortest-common-supersequence'
-        ]
-      }
-    }
-  }
-
+          "backpack-problem",
+          "cartesian-product",
+          "combinations",
+          "combined-sum",
+          "fisher–yates-permutation",
+          "longest-common-subsequence",
+          "longest-growing-subsequence",
+          "maximum-subarray",
+          "permutations",
+          "set-power",
+          "shortest-common-supersequence",
+        ],
+      };
+    },
+  };
 </script>
